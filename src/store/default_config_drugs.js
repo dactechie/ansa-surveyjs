6337ec8ce4b8c328a003932e3d07cd38548e0384
Aftab.Jalal <directionshealth.forms@gmail.com>
2020-10-02 16:55:45 +1000

    local-store drugs configuration

---
+++ src/store/default_config_drugs.js
@@ -0,0 +1,363 @@
+export default {
+  nsw_drugs_categories: [
+    {
+      category: "ANALGESICS",
+      drugs: ["Pharmaceutical Opioid, nfd"]
+    },
+    {
+      category: "ANALGESICS - Organic Opiate Analgesics",
+      drugs: ["Codeine", "Morphine", "Organic Opiate Analgesics, nec"]
+    },
+    {
+      category: "ANALGESICS − Semisynthetic Opioid Analgesics",
+      drugs: [
+        "Buprenorphine",
+        "Heroin",
+        "Oxycodone",
+        "Semisynthetic Opioid Analgesics, nec"
+      ]
+    },
+    {
+      category: "ANALGESICS − Synthetic Opioid Analgesics",
+      drugs: [
+        "Fentanyl",
+        "Fentanyl analogues",
+        "Levomethadyl acetate hydrochloride",
+        "Meperidine analogues",
+        "Methadone",
+        "Pethidine",
+        "Tramadol",
+        "Synthetic Opioid Analgesics, nec"
+      ]
+    },
+    {
+      category: "ANALGESICS −Non Opioid Analgesics (1400)",
+      drugs: [
+        "Acetylsalicylic acid",
+        "Paracetamol",
+        "Ibuprofen",
+        "Non Opioid Analgesics, nec"
+      ]
+    },
+    {
+      category: "SEDATIVES AND HYPNOTICS − Alcohols (2100)",
+      drugs: ["Ethanol", "Methanol", "Alcohols, nec"]
+    },
+    {
+      category: "SEDATIVES AND HYPNOTICS − Anaesthetics (2200)",
+      drugs: [
+        "Ketamine",
+        "Nitrous oxide",
+        "Phencyclidine",
+        "Propofol",
+        "Anaesthetics, nec"
+      ]
+    },
+    {
+      category: "SEDATIVES AND HYPNOTICS − Barbiturates (2300)",
+      drugs: [
+        "Amylobarbitone",
+        "Methylphenobarbitone",
+        "Phenobarbitone",
+        "Barbiturates, nec"
+      ]
+    },
+    {
+      category: "AND HYPNOTICS − Benzodiazepines (2400)",
+      drugs: [
+        "Alprazolam",
+        "Clonazepam",
+        "Diazepam",
+        "Flunitrazepam",
+        "Lorazepam",
+        "Nitrazepam",
+        "Oxazepam",
+        "Temazepam",
+        "Benzodiazepines, nec"
+      ]
+    },
+    {
+      category: "SEDATIVES AND HYPNOTICS − GHB Type Drugs and Analogues (2500)",
+      drugs: [
+        "Gamma-hydroxybutyrate",
+        "Gamma-butyrolactone",
+        "1,4-butanediol",
+        "GHB Type Drugs and Analogues, nec"
+      ]
+    },
+    {
+      category:
+        "SEDATIVES AND HYPNOTICS − Other Sedatives and Hypnotics (2900)",
+      drugs: [
+        "Chlormethiazole",
+        "Kava lactones",
+        "Zopiclone",
+        "Doxylamine",
+        "Promethazine",
+        "Zolpidem",
+        "Other Sedatives and Hypnotics, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS",
+      drugs: ["Psychostimulant, nfd"]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Amphetamines (3100)",
+      drugs: [
+        "Amphetamine",
+        "Dexamphetamine",
+        "Methamphetamine",
+        "Amphetamine analogues",
+        "Amphetamines, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Ephedra Alkaloids (3300)",
+      drugs: [
+        "Ephedrine",
+        "Norephedrine",
+        "Pseudoephedrine",
+        "Ephedra Alkaloids, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Phenethylamines (3400)",
+      drugs: [
+        "DOB",
+        "DOM",
+        "MDA",
+        "MDEA",
+        "MDMA",
+        "Mescaline",
+        "PMA",
+        "TMA",
+        "DOI",
+        "PMMA",
+        "2C-B",
+        "Phenethylamine analogues",
+        "Phenethylamines, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Tryptamines (3500)",
+      drugs: [
+        "Atropinic alkaloids",
+        "Diethyltryptamine",
+        "Dimethyltryptamine",
+        "Lysergic acid diethylamide",
+        "Psilocybin or Psilocin",
+        "Tryptamine analogues",
+        "Tryptamines, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Volatile Nitrates (3600)",
+      drugs: ["Amyl nitrate", "Butyl nitrate", "Volatile Nitrates, nec"]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Cathinones (3700)",
+      drugs: [
+        "Cathinone",
+        "Methcathinone",
+        "Cathinone analogues",
+        "Cathinones, nec"
+      ]
+    },
+    {
+      category: "STIMULANTS AND HALLUCINOGENS − Piperazines (3800)",
+      drugs: [
+        "1-Benzylpiperazine",
+        "1-(3-Trifluoromethylphenyl)-piperazine",
+        "1-(3-Chlorophenyl)-piperazine",
+        "Phenylpiperazine analogues",
+        "Piperazines, nec"
+      ]
+    },
+    {
+      category:
+        "STIMULANTS AND HALLUCINOGENS − Other Stimulants and Hallucinogens",
+      drugs: [
+        "Caffeine",
+        "Cocaine",
+        "Methylphenidate",
+        "Nicotine",
+        "Other Stimulants and Hallucinogens, nec"
+      ]
+    },
+    {
+      category:
+        "ANABOLIC AGENTS AND SELECTED HORMONES − Anabolic Androgenic Steroids",
+      drugs: [
+        "Boldenone",
+        "Dehydroepiandrosterone",
+        "Fluoxymesterone",
+        "Mesterolone",
+        "Methandriol",
+        "Methenolone",
+        "Nandrolone",
+        "Oxandrolone",
+        "Stanozolol",
+        "Testosterone",
+        "Anabolic Androgenic Steroids, nec"
+      ]
+    },
+    {
+      category: "ANABOLIC AGENTS AND SELECTED HORMONES − Beta2 Agonists",
+      drugs: ["Eformoterol", "Fenoterol", "Salbutamol", "Beta2 Agonists, nec"]
+    },
+    {
+      category:
+        "ANABOLIC AGENTS AND SELECTED HORMONES - Peptide Hormones, Mimetics and Analogues (4300)",
+      drugs: [
+        "Chorionic gonadotrophin",
+        "Corticotrophin",
+        "Erythropoietin",
+        "Growth hormone",
+        "Insulin",
+        "Peptide Hormones, Mimetics and Analogues, nec"
+      ]
+    },
+    {
+      category:
+        "ANABOLIC AGENTS AND SELECTED HORMONES − Other Anabolic Agents and Selected Hormones (4900)",
+      drugs: [
+        "Sulfonylurea hypoglycaemic agents",
+        "Tamoxifen",
+        "Thyroxine",
+        "Other Anabolic Agents and Selected Hormones, nec"
+      ]
+    },
+    {
+      category:
+        "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Monoamine Oxidase Inhibitors (5100)",
+      drugs: [
+        "Moclobemide",
+        "Phenelzine",
+        "Tranylcypromine",
+        "Monoamine Oxidase Inhibitors, nec"
+      ]
+    },
+    {
+      category: "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Phenothiazines (5200)",
+      drugs: [
+        "Chlorpromazine",
+        "Fluphenazine",
+        "Pericyazine",
+        "Thioridazine",
+        "Trifluoperazin",
+        "Phenothiazines, nec"
+      ]
+    },
+    {
+      category:
+        "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Serotonin Reuptake Inhibitors (5300)",
+      drugs: [
+        "Citalopram",
+        "Fluoxetine",
+        "Paroxetine",
+        "Sertraline",
+        "Serotonin Reuptake Inhibitors, nec"
+      ]
+    },
+    {
+      category: "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Thioxanthenes",
+      drugs: ["Flupenthixol", "Thiothixene", "Thioxanthenes, nec"]
+    },
+    {
+      category:
+        "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Tricyclic Antidepressants",
+      drugs: [
+        "Amitriptyline",
+        "Clomipramine",
+        "Dothiepin",
+        "Doxepin",
+        "Nortriptyline",
+        "Tricyclic Antidepressants, nec"
+      ]
+    },
+    {
+      category: "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Atypical Antipsychotics",
+      drugs: [
+        "Amisulpride",
+        "Aripriprazole",
+        "Clozapine",
+        "Olanzapine",
+        "Quetiapine",
+        "Risperidone",
+        "Ziprasidone",
+        "Atypical Antipsychotics, nec"
+      ]
+    },
+    {
+      category:
+        "ANTIDEPRESSANTS AND ANTIPSYCHOTICS − Other Antidepressants and Antipsychotics (5900)",
+      drugs: [
+        "Butyrophenones",
+        "Lithium",
+        "Mianserin",
+        "Other Antidepressants and Antipsychotics, nec"
+      ]
+    },
+    {
+      category: "VOLATILE SOLVENTS − Aliphatic Hydrocarbons (6100)",
+      drugs: ["Butane", "Petroleum", "Propane", "Aliphatic Hydrocarbons, nec"]
+    },
+    {
+      category: "VOLATILE SOLVENTS − Aromatic Hydrocarbons (6200)",
+      drugs: ["Toluene", "Xylene", "Aromatic Hydrocarbons, nec"]
+    },
+    {
+      category: "VOLATILE SOLVENTS − Halogenated Hydrocarbons (6300)",
+      drugs: [
+        "Bromochlorodifluoromethane",
+        "Chloroform",
+        "Tetrachloroethylene",
+        "Trichloroethane",
+        "Trichloroethylene",
+        "Halogenated Hydrocarbons, nec"
+      ]
+    },
+    {
+      category: "VOLATILE SOLVENTS − Other Volatile Solvents (6900)",
+      drugs: [
+        "Acetone",
+        "Ethyl acetate",
+        "Other Volatile Solvents, nec",
+        "CANNABINOIDS AND RELATED DRUGS −Cannabinoids and Related",
+        "Drugs (7100)",
+        "Cannabinoids",
+        "Cannabinoid agonists",
+        "Cannabinoids and Related Drugs, nec"
+      ]
+    },
+    {
+      category: "MISCELLANEOUS DRUGS OF CONCERN − Diuretics (9100)",
+      drugs: [
+        "Antikaliuretics",
+        "Loop diuretics",
+        "Thiazides",
+        "Diuretics, nec"
+      ]
+    },
+    {
+      category: "MISCELLANEOUS DRUGS OF CONCERN − Opioid Antagonists (9200)",
+      drugs: ["Naloxone", "Naltrexone", "Opioid Antagonists, nec"]
+    },
+    {
+      category: "MISCELLANEOUS DRUGS OF CONCERN − Laxatives (9300)",
+      drugs: ["Laxatives"]
+    },
+    {
+      category: "Others / Not Stated",
+      drugs: [
+        "MISCELLANEOUS DRUGS OF CONCERN − Other Drugs of Concern",
+        "Other Drugs of Concern",
+        "INADEQUATELY DESCRIBED (for Principal Drug of Concern only)",
+        "NOT STATED",
+        "NO DRUG OF CONCERN",
+        "GAMBLING"
+      ]
+    }
+  ]
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,17 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import * as drugs from "./default_config_drugs";
 Vue.use(Vuex);
 
+function setAndReturn(data) {
+  localStorage.setItem("config", JSON.stringify(data));
+  return drugs;
+}
+
 export default new Vuex.Store({
-  state: {},
+  state: {
+    config: JSON.parse(localStorage.getItem("config")) || setAndReturn(drugs)
+  },
   mutations: {},
   actions: {},
   modules: {}
