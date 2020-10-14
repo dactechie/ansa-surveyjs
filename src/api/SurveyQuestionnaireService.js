c29973e977e1392c8764bbe7f4f561aad05d77d4
Aftab.Jalal <directionshealth.forms@gmail.com>
2020-10-14 16:26:59 +1100

    Updated Survey IDs

--- src/api/SurveyQuestionnaireService.js
+++ src/api/SurveyQuestionnaireService.js
@@ -6,17 +6,16 @@ export default {
   async getSurveysNameID() {
     return [
       {
-        name: "ANSA InitialAssessment",
-        surveyid: "ddc3dec4-6de5-4bc9-a963-87e20896feed"
-      },
-      {
-        name: "ITSP Review V1",
-        surveyid: "2509f4ab-f7ad-4be0-993d-a4884362b68a"
-      },
-
-      {
         name: "ANSA ClientRegistration",
         surveyid: "ef269b86-8397-4d15-9a13-a46e683b9a12"
+      },
+      {
+        name: "ANSA InitialAssessment",
+        surveyid: "4f9a16b6-3ee2-4568-841f-194db4baef8c" //"ddc3dec4-6de5-4bc9-a963-87e20896feed"
+      },
+      {
+        name: "ITSP ReviewAssessment",
+        surveyid: "26c2c77a-e85d-43a3-b129-d4f70734fac7" //2509f4ab-f7ad-4be0-993d-a4884362b68a"
       }
     ];
 
