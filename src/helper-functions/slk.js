a455b119df131faf52a93c6ff4b12f28e6f6124f
Aftab.Jalal <directionshealth.forms@gmail.com>
2020-10-22 16:57:10 +1100

    Fixed SLK bug

--- src/helper-functions/slk.js
+++ src/helper-functions/slk.js
@@ -10,8 +10,8 @@ function getSLK(fname, lname, dob, sex) {
     s = lname.replace(/([^a-z-]+)/gi, ""),
     d = reverseCleanString(dob),
     x = sex;
-  f = f.toUpperCase().padEnd(2 - f.length, "9");
-  s = s.toUpperCase().padEnd(5 - f.length, "9");
+  f = f.toUpperCase().padEnd(3, "9");
+  s = s.toUpperCase().padEnd(5, "9");
   let name_part = `${s[1]}${s[2]}${s[4]}${f[1]}${f[2]}`;
 
   switch (x) {
