4764077fb7d2d412dbe57a17c1f729e4eea486c7
dactechie <directions.admin@directionshealth.com>
2024-10-17 04:53:11 +1300

    hard-coding configuration

--- src/api/AZTableService.js
+++ src/api/AZTableService.js
@@ -8,8 +8,12 @@ import {
 } from "@/common/constants";
 import { fromAZDataArray } from "@/common/AZDataAdapter";
 
-const GET_URL = process.env.VUE_APP_GET_TABLESTORE_LOGAPP;
-const UPSERT_URL = process.env.VUE_APP_UPSERT_TABLESTORE_LOGAPP;
+const GET_URL =
+  process.env.VUE_APP_GET_TABLESTORE_LOGAPP ||
+  "https://prod-05.australiaeast.logic.azure.com:443/workflows/bd761dc501c242ff8b9660bbe376f2f9/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=EWT2HvlbvI393jINSYDkzTS3orRmsUpOe6B4Y4DD8fI";
+const UPSERT_URL =
+  process.env.VUE_APP_UPSERT_TABLESTORE_LOGAPP ||
+  "https://prod-24.australiaeast.logic.azure.com:443/workflows/19d45fab43ae44b9bc34b2a9d77a09aa/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=uKMLzsdtlM9zstml81N1sXtnQjaIfeQ1z63FjopUSJ4";
 
 /**
  * In active mode
--- src/common/constants.js
+++ src/common/constants.js
@@ -1,14 +1,15 @@
 export const APPLICATION_MODE_NORMAL = 1;
 export const APPLICATION_MODE_ADMIN = 0;
 
-export const APP_ENVIRONMENT = process.env.VUE_APP_ENVIRONMENT || "prod";
+export const APP_ENVIRONMENT = process.env.VUE_APP_ENVIRONMENT || "staging";
 
-export const APP_AZSLOT = process.env.VUE_APP_AZSLOT || "prod";
+export const APP_AZSLOT = process.env.VUE_APP_AZSLOT || "staging";
 
 export const CLIENT_HISTORY_URL = process.env.VUE_APP_CLIENT_HISTORY_URL;
 
 export const SURVEY_IDS =
-  process.env.VUE_APP_SURVEYIDS_INAS_ITSP || "unknownids";
+  process.env.VUE_APP_SURVEYIDS_INAS_ITSP ||
+  "a756e7f0-2be0-4888-8326-e0fa97f9bf72_677c0e2a-1446-4dd6-bb61-4204ee693d79_8a3f9c55-c9ac-4824-9f82-6bf4bec93fae_708f01d7-20cb-4b98-948a-e7b8888bcbca_85e8b35d-5b10-4707-a550-8cb62ce1ea50_94c10760-f04d-4e5e-8ec3-1432c4cde1c8_aff3512e-057c-4792-b10a-872a584d2224_28b00db7-bb8a-4096-b326-04406806ba89";
 
 export const PARTITION_KEY = "PartitionKey";
 export const ROW_KEY = "RowKey";
@@ -20,11 +21,17 @@ export const DB_ENTITY_NAME = "ATOM";
 
 export const SLK_LENGTH = 14;
 
-export const PREFILL_EXCLUSIONS = process.env.VUE_APP_PREFILL_EXCLUSIONS || "";
-export const PREFILL_EXCLUSIONS_ALLCASES =
-  process.env.VUE_APP_PREFILL_EXCLUSIONS_ALLCASES || "";
+// export const PREFILL_EXCLUSIONS = process.env.VUE_APP_PREFILL_EXCLUSIONS || "";
+// export const PREFILL_EXCLUSIONS_ALLCASES =
+//   process.env.VUE_APP_PREFILL_EXCLUSIONS_ALLCASES || "";
 
-export const MANDATORY_FIELDS = process.env.VUE_APP_MANDATORY_FIELDS || "";
+// export const MANDATORY_FIELDS = process.env.VUE_APP_MANDATORY_FIELDS || "";
+
+export const PREFILL_EXCLUSIONS_ALLCASES = "SDS_Score,K10_Score,CDS5_Score";
+export const PREFILL_EXCLUSIONS =
+  "AssessmentType,doK10,SubstanceUseITSPIssues,SubstanceUseITSPGoals,EverydayLivingITSPIssues,EverydayLivingITSPGoals,HousingSafetyITSPIssues,HousingSafetyITSPGoals,PhysicalHealthITSPIssues,PhysicalHealthITSPGoals,MentalITSPIssues,MentalHealthITSPGoals,RelationshipsITSPIssues,RelationshipsITSPGoals,LegalITSPIssues,LegalITSPGoals,SDSIsAODUseOutOfControl,SDSDoesMissingFixMakeAnxious,SDSHowMuchDoYouWorryAboutAODUse,SDSDoYouWishToStop,SDSHowDifficultToStopOrGoWithout,doK10,K10Q01,K10Q02,K10Q03,K10Q04,K10Q05,K10Q06,K10Q07,K10Q08,K10Q09,K10Q10,CDS5RateAddictiontoCigarettes,CDS5HowManyCigarettesPerDay,CDS5HowSoonAfterWaking,CDS5HowHardIsQuitting,CDS5HowStrongTheUrgeToSmoke,Past4WkDailyLivingImpacted,Past4WkDifficultyFindingHousing,Past4WkPhysicalHealth,Past4WkHowOftenPhysicalHealthCausedProblems,Past4WkMentalHealth,Past4WkHowOftenMentalHealthCausedProblems,Past4WkUseLedToProblemsWithFamilyFriend,Past4WkHowOftenIllegalActivities,Past4WkQualityOfLifeScore";
+export const MANDATORY_FIELDS =
+  "SDSIsAODUseOutOfControl,SDSDoesMissingFixMakeAnxious,SDSHowMuchDoYouWorryAboutAODUse,SDSDoYouWishToStop,SDSHowDifficultToStopOrGoWithout,Past4WkDailyLivingImpacted,Past4WkDifficultyFindingHousing,DoYouFeelSafeWhereYouLive,Past4WkPhysicalHealth,Past4WkHowOftenPhysicalHealthCausedProblems,Past4WkMentalHealth,Past4WkHowOftenMentalHealthCausedProblems,Past4WkUseLedToProblemsWithFamilyFriend,HaveDVOrFamilySafetyConcerns,Past4WkHowOftenIllegalActivities,HowCloseToManagingSubstanceUse,HowCloseToManagingImpactOfOthersUse";
 
 // export const PREFILL_EXCLUSION_PREFIXES = (
 //   process.env.VUE_APP_PREFILL_EXCLUSION_PREFIXES || []
